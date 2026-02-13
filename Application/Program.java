4a4cb0fb4ee7ca5af1ab9746850685fac628a2f9
João pedro Floriano <joaopflori23@gmail.com>
2026-02-13 15:34:54 -0300

    Update Implementation

--- Application/Program.java
+++ Application/Program.java
@@ -2,7 +2,7 @@ package Application;
 
 import java.sql.Connection;
 import java.time.LocalDate;
-import java.util.List;
+
 
 import db.DB;
 import model.dao.DaoFactory;
@@ -13,14 +13,13 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args){
-        Seller sel = new Seller(null, "Lula", "Lulaladrao@gmail.com", LocalDate.parse("2006-06-04"), 2000.0, new Department(2, "Eletronics"));
+        Seller sel = new Seller(10, "Lula", "bababoi@gmail.com", LocalDate.parse("2006-06-04"), 2000.0, new Department(2, "Eletronics"));
         Connection conn = DB.getConnection();
 
-        DaoFactory d = new DaoFactory();
-        SellerDao sd = d.createSellerDao(conn);
-        sd.insert(sel);
-        System.out.println("Inserted! New id = " + sel.getId());
-
+        sel.setEmail("peitanao@gmail.com");
+        DaoFactory factory = new DaoFactory();
+        SellerDao sellerDao = factory.createSellerDao(conn);
+        sellerDao.update(sel);
 
         
     }
